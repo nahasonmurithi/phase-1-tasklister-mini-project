05e52964969fe80619327a8166c987640a2176cc
Nahason <nahason.murithi@student.moringaschool.com>
2023-10-09 19:15:28 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+const formData = document.querySelector("#create-task-form");
+
+checkbox.addEventListener("click", checkboxClick);
+
+function checkboxClick(event) {
+  let toDoItems = text;
+  document.getElementById("output-box").innerHTML += text;
+  event.preventDefault();
+}
 });
